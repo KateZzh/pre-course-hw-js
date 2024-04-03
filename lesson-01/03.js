825fcccc7eedff2c4f4647437cc98bf885fff8ac
KateZzh <ekaterin.pro2@gmail.com>
2024-04-03 23:35:35 +0300

    added lesson-01/03

---
+++ lesson-01/03.js
@@ -0,0 +1,9 @@
+// --number   number--  ++number   number++
+// Получи в консоли 1,3,3,1
+
+let number = 1;
+
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
